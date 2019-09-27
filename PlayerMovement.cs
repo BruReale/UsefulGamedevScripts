50dc775f58c3c13cfb8778cfb7dbcebf84203aff
Unknown <puerto.contactos@gmail.com>
2019-09-26 22:55:01 -0300

    3D Player Movement
    Player Movement 3D third person, Mouse X scrolling

---
+++ PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed;
+    public float rotSpeed;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity =
+            Input.GetAxis("Vertical") * 
+            transform.forward * 
+            speed * 
+            Time.deltaTime 
+            
+            + 
+
+            Input.GetAxis("Horizontal") * 
+            transform.right * 
+            speed * 
+            Time.deltaTime;
+
+        transform.Rotate 
+            (0, Input.GetAxis("Mouse X") * 
+            rotSpeed * 
+            Time.deltaTime, 0);
+    }
+}
