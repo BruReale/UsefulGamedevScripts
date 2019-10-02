63910ca57b6c692c4d53d0d7413278081ae38f41
Unknown <puerto.contactos@gmail.com>
2019-10-01 23:11:17 -0300

    Added 8-direction 2D movement

---
+++ EightDirections.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EightDirections : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2D;
+
+   //basic 8-direction movement with RigidBody2D and velocity set
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb2D.velocity = new Vector2
+                (Input.GetAxis("Horizontal") * speed * Time.deltaTime,
+                Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
