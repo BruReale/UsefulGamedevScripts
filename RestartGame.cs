6f09124b94bd1e0d7d726c4859b1ddd45c4408f7
Unknown <puerto.contactos@gmail.com>
2019-10-03 21:21:42 -0300

    changed something i dont recall

---
+++ RestartGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGameEscaperoom : MonoBehaviour
+{
+    public float timer;
+    public float timerUntilRestart;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+
+        if (timer >= timerUntilRestart)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
